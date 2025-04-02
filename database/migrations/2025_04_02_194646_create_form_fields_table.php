cc67d2b72c1894aac4548999b6173521602ce67b
Benjamin Benkert <29586970+bbenkert@users.noreply.github.com>
2025-04-02 15:48:23 -0400

    Define Schema in Migration Files

---
+++ database/migrations/2025_04_02_194646_create_form_fields_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form_fields', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('form_id')->constrained()->onDelete('cascade');
+            $table->string('label');
+            $table->string('name');
+            $table->string('type'); // e.g., text, textarea, select
+            $table->json('options')->nullable(); // for dropdowns or radios
+            $table->boolean('required')->default(false);
+            $table->integer('order')->default(0);
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form_fields');
+    }
+};
