639560d2305278eb90c29a78d4ecc5314821dfb7
Benjamin Benkert <29586970+bbenkert@users.noreply.github.com>
2025-04-03 15:39:00 -0400

    Add /office homepage with controller and view

---
+++ resources/views/office/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Old Town Church Office</title>
+</head>
+<body>
+    <h1>Welcome to the Office Portal</h1>
+    <p>This is where forms will be managed.</p>
+</body>
+</html>
