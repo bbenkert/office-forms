3afe76c27c713fafe3153b5c9c30e4f7ddd81aa6
Benjamin Benkert <29586970+bbenkert@users.noreply.github.com>
2025-04-03 15:42:35 -0400

    Replace Laravel welcome page with /office homepage

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OfficeHomeController;
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/office', [OfficeHomeController::class, 'index']);
-
+Route::get('/', [OfficeHomeController::class, 'index']);
